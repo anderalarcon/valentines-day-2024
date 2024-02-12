5314188205edc71473855d3af976dbc2e3eef0fe
Ander <20172991@aloe.ulima.edu.pe>
2024-02-12 17:15:24 -0500

    more fixes

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -23,21 +23,25 @@ export default function RootLayout({ children }) {
     <html lang="en" className={myFont.className}
     >
       <head>
+        <link rel="canonical" href="https://valentines-day-2024-m8rz0rd0k-anderalarcon.vercel.app/" />
+        <meta property="og:type" content="website" />
         <meta property="og:title" content="San Valentin 2024" />
         <meta property="og:description" content="Sorprende a tu pareja este San Valentin" />
         <meta property="og:image"
           content="https://i.pinimg.com/originals/2b/18/1c/2b181cbe4bc9e1cdf33e39d7ce2df5fd.jpg" />
-        <meta property="og:url" content="URL_DE_TU_LANDING_PAGE" />
+        <meta property="og:url" content="https://valentines-day-2024-m8rz0rd0k-anderalarcon.vercel.app/" />
         <meta name="twitter:title" content="San Valentin 2024" />
         <meta name="twitter:description" content="Sorprende a tu pareja este San Valentin" />
         <meta name="twitter:image"
           content="https://i.pinimg.com/originals/2b/18/1c/2b181cbe4bc9e1cdf33e39d7ce2df5fd.jpg" />
+        <meta name="twitter:card" content="summary_large_image" />
+
       </head>
       <body>
         <Suspense>
           {children}
         </Suspense>
-      s</body>
+      </body>
     </html>
   );
 }
